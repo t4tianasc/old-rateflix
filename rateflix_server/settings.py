60b5140644f794bd8414a1924fe5df66d1d48c80
Tatiana Sanchez <tatianasanchez03@gmail.com>
2023-11-24 10:39:50 +0100

    Add movie project to the app

--- rateflix_server/settings.py
+++ rateflix_server/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movies.apps.MoviesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
